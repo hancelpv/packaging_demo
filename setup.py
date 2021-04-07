b12edd458968e09fbb0b5cf452109182be0ba750
hancel.pv <hancel.pv@corecompete.com>
2021-04-07 10:31:45 +0530

    adding package

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup
 setup(
     name='hello-world',
     version='1.0.0',
-    description='Sample Hello World code'
+    description='Sample Hello World code',
+    packages=['my_package']
 )
