8afa70c4735c0ec232bd5b441c2cff7883505c45
hancel.pv <hancel.pv@corecompete.com>
2021-04-07 10:26:25 +0530

    Adding sample code

---
+++ my_package/module.py
@@ -0,0 +1,14 @@
+class HelloWorld:
+    """
+    Hello world class
+
+    Methods available :
+        print()
+    """
+
+    def __init__(self):
+        pass
+
+    def print(self) -> None:
+        """Prints hello world to std out"""
+        print("Hello World !")
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='hello-world',
+    version='1.0.0',
+    description='Sample Hello World code'
+)
